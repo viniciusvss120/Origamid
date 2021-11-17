0d6e21121285fbb1e937953577a20a87e08d73fe
Vinicius Silva Souza <vinicius100@live.com>
2021-11-17 15:40:51 -0400

    Update script.js

--- javaScript/Dom-para-iniciantes/animais-fantasticos copy/script.js
+++ javaScript/Dom-para-iniciantes/animais-fantasticos copy/script.js
@@ -1,5 +1,45 @@
+/*
 const animais = document.getElementById('animais')
 console.log(animais)
 
 const gridSection = document.getElementsByClassName('grid-section')
 console.log(gridSection[0])
+
+
+const primeiraLi= document.querySelector('li')
+
+console.log(primeiraLi)
+
+const linksInternos = document.querySelector('[href^="#"]') 
+console.log(linksInternos)
+
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg[1])
+
+const gridSectionHTML = document.getElementsByClassName('grid-section')
+const gridSectionNode = document.querySelectorAll('.grid-section')
+
+
+primeiraLi.classList.add('grid-section')
+
+console.log(gridSectionHTML[0])
+console.log(gridSectionNode[0])
+
+gridSectionNode.forEach(function(item, index){
+  console.log(item)
+})
+
+const arrayGrid = Array.from(gridSectionHTML)
+
+*/
+
+///////---/////
+
+//1
+const imagens = document.querySelectorAll('.animais img')
+console.log(imagens)
+
+//2
+const retornaImg = document.querySelectorAll('.animais imagem')
+console.log(retornaImg)
