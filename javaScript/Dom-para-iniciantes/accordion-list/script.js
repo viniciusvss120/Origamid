e2a8041054c98be4e293bde8a2a73b197cf41746
Vinicius Silva Souza <vinicius100@live.com>
2021-11-23 15:04:40 -0400

    sequencia do projeto

---
+++ javaScript/Dom-para-iniciantes/accordion-list/script.js
@@ -0,0 +1,41 @@
+function initTabNav(){
+  const tabMenu = document.querySelectorAll('.js-tabmenu li')
+  const tabContent = document.querySelectorAll('.js-tabcontent section')
+  
+  if(tabMenu.length && tabContent.length){
+    tabContent[0].classList.add('ativo')
+  
+    function activeTab(index){
+      tabContent.forEach((section) =>{
+        section.classList.remove('ativo')
+      })
+      tabContent[index].classList.add('ativo')
+    }
+  
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', function(){
+        activeTab(index)
+      })
+    })
+  }
+}
+
+initTabNav()
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  const ativoClass = 'ativo'
+  if(accordionList.length)
+  accordionList[0].classList.add(ativoClass)
+  accordionList[0].nextElementSibling.classList.add(ativoClass)
+  
+  function activeAccordion(){
+    this.classList.toggle(ativoClass)
+    this.nextElementSibling.classList.toggle(ativoClass)
+  }
+  
+  accordionList.forEach((item) =>{
+    item.addEventListener('click', activeAccordion)
+  })
+}
+initAccordion()
