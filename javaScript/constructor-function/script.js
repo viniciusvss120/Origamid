830f484fa0bd9e2c55c540e8b9c9f5256043e219
Vinicius Silva Souza <vinicius100@live.com>
2021-11-23 23:23:47 -0400

    sequencia da aula

--- javaScript/constructor-function/script.js
+++ javaScript/constructor-function/script.js
@@ -7,15 +7,15 @@ const honda = carro
 honda.marca = 'Honda'
 honda.preco = 2000 */
 
-function Carro(){
-    this.marca = 'Marca'
-    this.preco = 2000
+function Carro(marcaAtribuida, precoAtribuido){
+    this.marca = marcaAtribuida
+    this.preco = precoAtribuido
 }
 
-const honda = new Carro()
-const fiat = new Carro()
-fiat.marca = 'Fiat'
-fiat.preco = 3000
+const honda = new Carro('Honda', 3000)
+const fiat = new Carro('Fiat', 4000)
+
+console.log(honda)
 
     
 
