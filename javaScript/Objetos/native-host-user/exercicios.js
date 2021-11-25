367d16363b48cf3351c626047d42440661a8dc88
Vinicius Silva Souza <vinicius100@live.com>
2021-11-24 23:14:47 -0400

    sequencia

---
+++ javaScript/Objetos/native-host-user/exercicios.js
@@ -0,0 +1,22 @@
+//1 
+Object
+Array
+Function
+String
+Number
+
+//2
+Window
+Document
+HTMLCollection
+NodeList
+History
+
+//3
+
+if(typeof document.adoptNode !== "undefined"){
+    console.log('existe')
+} else {
+    console.log ('Deu ruim')
+}
+
