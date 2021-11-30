117aab4b45e55ccb2e881a16fca1e1c83eaf2e8b
Vinicius Silva Souza <vinicius100@live.com>
2021-11-30 09:59:45 -0400

    sequencia aula

--- javaScript/Efeitos-no-dom/modal/js/script.js
+++ javaScript/Efeitos-no-dom/modal/js/script.js
@@ -2,15 +2,12 @@ import initScrollSuave from './modules/scroll-suave.js'
 import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
-import * as teste from './modules/teste.js'
+import initiModal from './modules/modal.js'
 
-
-teste.teste1()
-teste.teste2()
-console.log(teste.senha)
 
 
 initScrollSuave()
 initAnimacaoScroll()
 initAccordion()
 initTabNav()
+initiModal()
