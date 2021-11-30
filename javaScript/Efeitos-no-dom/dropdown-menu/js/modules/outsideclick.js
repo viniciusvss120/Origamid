6176021fda531b9e13c57b0ec97a5163a6fb3337
Vinicius Silva Souza <vinicius100@live.com>
2021-11-30 17:29:34 -0400

    sequencia aula

---
+++ javaScript/Efeitos-no-dom/dropdown-menu/js/modules/outsideclick.js
@@ -0,0 +1,24 @@
+export default function outsideClick(element, events ,calback){
+  const html = document.documentElement
+  const outside = 'data-outside'
+
+  if(!element.hasAttribute(outside)){
+    events.forEach(userEvent => {
+      html.addEventListener('click', handleOutsideClick)
+
+    })
+    element.setAttribute(outside, '')
+  }
+  function handleOutsideClick(event){
+    //console.log(event.target)
+    if(!element.contains((event.target))){
+      events.forEach(userEvent => {
+        element.removeAttribute(outside, '') 
+      })
+
+      html.removeEventListener('click', handleOutsideClick)
+      calback()
+
+    }
+  }
+}
