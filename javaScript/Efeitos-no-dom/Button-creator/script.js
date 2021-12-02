fe73f9eda4b946b0feda53f12924b9e3256d0c28
Vinicius Silva Souza <vinicius100@live.com>
2021-12-01 23:54:25 -0400

    sequencia

--- javaScript/Efeitos-no-dom/Button-creator/script.js
+++ javaScript/Efeitos-no-dom/Button-creator/script.js
@@ -39,8 +39,26 @@ function handleChange(event){
   const value = event.target.value
 
   handleStyle[name](value)
+  saveValues(name, value)
   showCss()  
 }
+
+function saveValues(name, value){
+  localStorage[name] = value
+}
+
+function setValues(){
+  const properties = Object.keys(localStorage)
+  properties.forEach(properties => {
+    handleStyle[properties](localStorage[properties])
+    controles.elements[properties].value = localStorage[properties]
+  })
+  showCss()
+}
+
+setValues()
+
+
 
 function showCss(){
   cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
