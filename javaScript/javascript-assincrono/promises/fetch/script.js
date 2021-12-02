91d748ca79ada77c1a790e74e960d76cd84d75e6
Vinicius Silva Souza <vinicius100@live.com>
2021-12-02 08:26:43 -0400

    sequencia aula

---
+++ javaScript/javascript-assincrono/promises/fetch/script.js
@@ -0,0 +1,18 @@
+const imagem = fetch('https://viacep.com.br/ws/76890000/json/')
+
+
+//imagem.then(response => {
+//    console.log(response)
+//    response.headers.forEach(console.log) 
+//    
+//})
+imagem.then(response => {
+    console.log(response.status)
+     
+    
+})
+
+const array = ['item 1', 'item 2']
+
+
+
