329997d5b12877326b3425fcebc6026699e4ef82
Vinicius Silva Souza <vinicius100@live.com>
2022-03-28 23:12:47 -0400

    Update animacoes.js

--- javaScript/Efeitos-no-dom/animacao-ao-scroll/animacoes.js
+++ javaScript/Efeitos-no-dom/animacao-ao-scroll/animacoes.js
@@ -77,15 +77,16 @@ function initAnimacaoScroll(){
   
   if(sections.length){
     const windowMetade = window.innerHeight * 0.6
+    console.log(windowMetade)
     
     function animaScroll(){
       sections.forEach((section) =>{
         const sectionTop = section.getBoundingClientRect().top
         const isSectionVisible = (sectionTop - windowMetade) < 0
-        
         if(isSectionVisible) {
           section.classList.add('ativo')
         }
+    
       })
     }
     animaScroll()
